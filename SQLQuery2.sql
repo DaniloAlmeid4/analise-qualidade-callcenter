1ad4b426f2da7d1b9cc831aff68b294ba5a72343
DaniloAlmeid4 <daniloalmeidadosssantos@gmail.com>
2026-05-26 19:04:36 -0300

    feat: projeto análise qualidade call center

--- SQLQuery2.sql
+++
@@ -1,5 +0,0 @@
-SELECT 'operadores' AS tabela, COUNT(*) AS total FROM dbo.operadores
-UNION ALL
-SELECT 'atendimentos', COUNT(*) FROM dbo.atendimentos
-UNION ALL
-SELECT 'monitorias', COUNT(*) FROM dbo.monitorias;
